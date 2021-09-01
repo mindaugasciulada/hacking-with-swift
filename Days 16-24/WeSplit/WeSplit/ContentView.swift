191de5a80c4ae1e97e24f71b2488496500713c20
Mindaugas <>
2021-09-01 11:00:21 +0300

    Add sections, people amount selection

--- Days 16-24/WeSplit/WeSplit/ContentView.swift
+++ Days 16-24/WeSplit/WeSplit/ContentView.swift
@@ -2,15 +2,70 @@
 //  ContentView.swift
 //  WeSplit
 //
-//  Created by User on 2021-09-01.
+//  Created by Mindaugas Čiulada on 2021-08-28.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var price: String = ""
+    @State private var selectedAmountOfPeople: Int = 0
+    @State private var selectedPercentage: Int = 2
+    
+    let navigationTitle = "WeSplit"
+    let selectAmountOfPeopleTitle = "Select amount of people"
+    let selectPercentageTitle = "Select percentage to tip"
+    let amountPerPersonTitle = "Amount per person"
+    let totalAmountForCheck = "Original amount + tip"
+    let percentages = [0, 10, 20, 30, 50]
+    
+    var dividedSum: Double {
+        let sum = Double(price) ?? 0
+        let people = Double(selectedAmountOfPeople + 2)
+        let percentage = sum / people * Double(percentages[selectedPercentage]) / 100
+        return sum / people + percentage
+    }
+    
+    var totalSum: Double {
+        let sum = Double(price) ?? 0
+        let percentage = sum * Double(percentages[selectedPercentage]) / 100
+        return sum + percentage
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Price", text: $price).keyboardType(.numberPad)
+                    Picker(selectAmountOfPeopleTitle, selection: $selectedAmountOfPeople){
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    Text("\(selectedAmountOfPeople) people")
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                }
+                
+                Section(header: Text(selectPercentageTitle).textCase(.none)) {
+                    Picker("Tip percentage", selection: $selectedPercentage) {
+                        ForEach(0..<percentages.count) {
+                            Text("\(percentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text(amountPerPersonTitle).textCase(.none)) {
+                    Text(String(format: "%.2f", dividedSum))
+                }
+                
+                Section(header: Text(totalAmountForCheck).textCase(.none)) {
+                    Text(String(format: "%.2f", totalSum))
+                }
+            }
+            .navigationTitle(navigationTitle)
+        }
+        
     }
 }
 
