437065386c08411ff0b019bacb32b4280e244545
Mindaugas <>
2021-09-01 10:26:54 +0300

    initial commit

---
+++ Days 16-24/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by User on 2021-09-01.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
